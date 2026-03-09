a23f532a846872b2071c20b71a12ce426218804d
tebp-stack <tebp@cin.ufpe.br>
2026-03-08 21:43:03 -0300

    Acrescimo de comentários

--- Lista 1/atividade_2.c
+++ Lista 1/atividade_2.c
@@ -2,14 +2,17 @@
 
 int main()
 {
+    // Uso do tipo long long para evitar overflow de variavel
     long long N, A, B, soma;
     long long multiploA, multiploB, multiploAB;
 
     scanf("%lld", &N);
     scanf("%lld %lld", &A, &B);
 
+    // Soma de 1 até o número N pela formula de P.A
     soma = (1 + N) * N / 2;
 
+    // Condições de multiplos de A, B e AB
     multiploA = A * (N / A) * ((N / A) + 1) / 2;
     multiploB = B * (N / B) * ((N / B) + 1) / 2;
     multiploAB = (A * B) * (N / (A * B)) * ((N / (A * B)) + 1) / 2;
